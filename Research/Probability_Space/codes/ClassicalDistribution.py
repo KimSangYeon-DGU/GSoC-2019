69f3706e50c0208474fec2e9c1dbbf6cbd765ce1
Kim SangYeon <sy0814k@gmail.com>
2019-05-23 15:39:28 +0900

    Add ignore and volume calculation

--- Research/Probability_Space/codes/ClassicalDistribution.py
+++ Research/Probability_Space/codes/ClassicalDistribution.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.mlab as mlab
 import numpy as np
 from Utils import *
+from scipy.integrate import dblquad
 
 def pdf(x, mean, cov):
     dim, mean, cov = process_parameters(None, mean, cov)
@@ -11,6 +12,16 @@ def pdf(x, mean, cov):
     out = np.exp(logpdf(x, mean, prec_U, log_det_cov))
 
     return squeeze_output(out)
+
+def getVolume(X, Y, Z):
+  
+  v = abs(X[0] * Y[0] * Z[0])
+  dim = X.shape[0]
+  for j in range(1, dim):
+    for i in range(1, dim):
+      v += (X[j][i] - X[j][i-1]) * (Y[j][i] - Y[j-1][i]) * Z[j][i]
+
+  return np.sum(v / dim)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -41,7 +52,15 @@ obs[:, :, 0] = X; obs[:, :, 1] = Y
 
 # Generate Gaussian distributions according to the parameters
 G1 = pdf(obs, mean1, cov1)
+
+# Calculate volume of G1
+v1 = getVolume(X, Y, G1)
+print("The volume of the first Gaussian: {0}".format(v1))
+
+# Calculate volume of G2
 G2 = pdf(obs, mean2, cov2)
+v2 = getVolume(X, Y, G2)
+print("The volume of the second Gaussian: {0}".format(v2))
 
 # Gaussian Mixture
 G_mix = w[0]*G1 + w[1]*G2
