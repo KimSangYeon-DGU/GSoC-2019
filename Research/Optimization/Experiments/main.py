ecd17039c70743cc538788ef5fbf93d37597693c
KimSangYeon-DGU <sy0814k@gmail.com>
2019-07-02 07:20:19 +0900

    Add QGMM

--- Research/Optimization/Experiments/main.py
+++ Research/Optimization/Experiments/main.py
@@ -1,70 +1,121 @@
 from qgmm_utils import *
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+from matplotlib.patches import Ellipse
 
+df = pd.read_csv('faithful.csv', sep=',')
+dataset = df.to_numpy()
+dataset = np.transpose(dataset)
+#print(dataset.T.shape)
+CLUSTERS = 2
+
+def eigsorted(cov):
+    '''
+    Eigenvalues and eigenvectors of the covariance matrix.
+    '''
+    vals, vecs = np.linalg.eigh(cov)
+    order = vals.argsort()[::-1]
+    return vals[order], vecs[:, order]
+
+
+def cov_ellipse(points, cov, nstd):
+    """
+    Source: http://stackoverflow.com/a/12321306/1391441
+    """
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:, 0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+
+    return width, height, theta
+
+def plot_clustered_data(points, c_means, covs):
+    """Plots the cluster-colored data and the cluster means"""
+    colors = cm.rainbow(np.linspace(0, 1, CLUSTERS))
+    #ax = plt.gca()
+    for i in range(points.shape[1]):
+      plt.plot(points[:, i][0], points[:, i][1], ".", color="red", zorder=0)
+    
+    plt.plot(c_means[0][0], c_means[0][1], ".", color="green", zorder=1)
+    plt.plot(c_means[1][0], c_means[1][1], ".", color="blue", zorder=1)
+
+    #width1, height1, theta1 = cov_ellipse(points, covs[0], nstd=0.3)
+    #ellipse1 = Ellipse(xy=(c_means[0][0], c_means[0][1]), width=width1, height=height1, angle=theta1,
+    #                   edgecolor='b', fc='None', lw=2, zorder=4)
+    #ax.add_patch(ellipse1)
+    plt.show()
+
+# Set the number of components is 2.
 num_components = 2
 
 alphas = tf.Variable([0.3, 0.7], dtype=tf.float32)
-means = tf.Variable([[1.0, 1.0], [0.0, 0.0]], dtype=tf.float32)
-covs = tf.Variable([[[1.0, 0.0], [0.0, 1.0]],[[2.0, 0.0],[0.0, 2.0]]], dtype=tf.float32)
+means = tf.Variable([[1.67, 50.89705882], [4.88778309, 80.89705882]], dtype=tf.float32)
+covs = tf.Variable([[[8, 0], [0, 8]],[[10, 0],[0, 10]]], dtype=tf.float32)
+
 covs_lower = tf.linalg.cholesky(covs)
 
-obs = np.array([[1, 2, 3, 4, 5],
-                [2, 3, 4, 5, 6]])
+#obs = tf.constant([[5, 6, 12, 4, 5],
+#                   [-2, -2, -3, -2, -1]], dtype=tf.float32)
 
+obs = tf.convert_to_tensor(dataset, dtype=tf.float32)
+
+'''
+# Build session
 sess = tf.InteractiveSession()
 init = tf.global_variables_initializer()
+
 sess.run(init)
-
-'''
-for i in range(alphas.shape[0]):
-  cov = tf.transpose(covs_lower[i]) * covs_lower[i]
-  print(sess.run(cov))
-'''
-cov = tf.transpose(covs_lower[0]) * covs_lower[0]
-print(sess.run(log_probability(obs, means[0], cov, 0.25)))
-#print(unnormalized_gaussians(obs, means[0], cov))
-
-#G = unnormalized_gaussians(obs, means[0], covs[0])
-#print(sess.run(tf_alphas[0]))
+print(sess.run(factor_covariance(covs[0])))
 '''
 G = []
-for alpha, mean, cov_lower in zip(alphas, means, covs_lower):
-  cov = np.dot(cov_lower.T, cov_lower)
-  G.append(unnormalized_gaussians(obs, mean, cov))
-'''
-'''
-Q = get_Q(G, alphas)
-G = np.asarray(G)
-Q = np.asarray(Q)
-ld = 0.1
+for i in range(num_components):
+  #cov = tf.matmul(tf.transpose(covs_lower[i]), covs_lower[i])
+  G.append(unnormalized_gaussians(obs, means[i], covs[i]))
 
 G = tf.convert_to_tensor(G, dtype=tf.float32)
-Q = tf.convert_to_tensor(Q, dtype=tf.float32)
+Q = get_Q(G, alphas, num_components)
 
-J = tf.reduce_sum(Q[0] * tf.log(G[0]) + Q[1] * tf.log(G[1]))# + ld * (((tf_alphas[0] ** 2) + (tf_alphas[1] ** 2) + 2 * tf_alphas[0] * tf_alphas[1] * get_cosine(G, tf_alphas) * np.sum(G[0] * G[1])) - 1)
+# lambda
+ld = 1
 
+# Objective function :: Minimize (NLL + lambda * approximation constant)
+# Approximation constant :: (Sum of P) - 1 = 0
+J = tf.reduce_sum(Q[0] * tf.math.log(G[0]) + Q[1] * tf.math.log(G[1])) + ld * (((alphas[0] ** 2) + (alphas[1] ** 2) + 2 * alphas[0] * alphas[1] * get_cosine(G, alphas) * tf.reduce_sum(G[0] * G[1])) - 1)
+
+# Set optimizer to Adam with learning rate 0.1
+optim = tf.train.AdamOptimizer(learning_rate=0.001)
+training_op = optim.minimize(-J)
+
+# Build session
 sess = tf.InteractiveSession()
 init = tf.global_variables_initializer()
 
-ALPHA, MEAN, COV, C = [], [], [], []
 sess.run(init)
-n_iter = 2000
 
-optim = tf.train.RMSPropOptimizer(learning_rate=0.01)
-training_op = optim.minimize(-J)
-
-
+# Set the number of iterations is 2000.
+n_iter = 200
+print(sess.run(covs))
 for i in range(n_iter):
     _ = sess.run(training_op)
-    ALPHA.append(sess.run(tf_alphas))
-    MEAN.append(sess.run(tf_means))
-    COV.append(sess.run(tf_covs_lower))
-    C.append(sess.run(J))
+    sess.run(alphas)
+    sess.run(means)
+    sess.run(covs)
+    sess.run(J)
+    #print(i, J.eval())
+    print(i, means.eval())
+  
+#covs = assemble_covs(covs_lower, num_components)
 
-# Get the final values    
-print('after {} iterations:'.format(n_iter))
-print('Cost: {} at Alpha={}, Mean={}, Cov={}'.format(J.eval(), tf_alphas.eval(), tf_means.eval(), tf_covs_lower.eval()))
+# Check the trained parameters with actual mean and covariance using numpy
+print('Cost: {}, alpha={}, mean={}, cov={}'.format(J.eval(), alphas.eval(), means.eval(), covs.eval()))
 
+print("Actual mean {0}".format(np.mean(dataset, 1)))
+print("Actual cov {0}".format(np.cov(dataset)))
+
+plot_clustered_data(dataset, means.eval(), covs.eval())
 sess.close()
-'''
--- Research/Optimization/Experiments/qgmm_utils.py
+++ Research/Optimization/Experiments/qgmm_utils.py
@@ -3,79 +3,106 @@ import tensorflow as tf
 
 log2pi = 1.83787706640934533908193770912475883
 
+def apply_positive_definite_constraint(covariance, num_components):
+  eigval, eigvec = tf.linalg.eigh(covariance)
+  
+  eig_min = tf.reduce_min(eigval)
+  eig_max = tf.reduce_max(eigval)
+  
+  def true():
+    minEigval = tf.math.maximum(eig_max / 1e5, 1e-50)
+
+    tmp_eigval = []
+    for i in range(num_components):
+      tmp_eigval.append(tf.math.maximum(eigval[i], minEigval))
+    
+    cov = tf.matmul(tf.matmul(eigvec, tf.diag(tmp_eigval)), tf.transpose(eigvec))
+    return cov
+
+  def false():
+    return covariance
+
+  cov = tf.cond(eig_min < 0.0, true, false)
+  cov = tf.cond(eig_max / eig_min < 0.0, true, false)
+  cov = tf.cond(eig_max  < 1e-50, true, false)    
+
+  return cov
+
 def factor_covariance(covariance):
-  cov_lower = tf.linalg.cholesky(covariance)
-  inv_cov_lower = tf.linalg.inv(cov_lower)
+  #cov = apply_positive_definite_constraint(covariance, 2)
+  #cov_lower = tf.linalg.cholesky(cov)
+  
+  #inv_cov_lower = tf.linalg.inv(cov_lower)
 
-  inv_cov = tf.transpose(inv_cov_lower) * inv_cov_lower
-  _, log_det_cov = tf.linalg.slogdet(cov_lower)
-
-  log_det_cov *= 2
+  #inv_cov = tf.transpose(inv_cov_lower) * inv_cov_lower
+  #_, log_det_cov = tf.linalg.slogdet(cov_lower)
+  #log_det_cov *= 2
+  
+  inv_cov = tf.diag((1 / tf.diag_part(covariance)))
+  log_det_cov = tf.reduce_sum(tf.math.log(tf.diag_part(covariance)))
 
   return inv_cov, log_det_cov
 
 def log_probability(observations, mean, covariance, c):
   observations = tf.transpose(observations)
-  
-  k = tf.shape(observations)[1]
-  
+  k = tf.cast(tf.shape(observations)[1], tf.float32)
+
   inv_cov, log_det_cov = factor_covariance(covariance)
   
-  diff = (observations - mean)
-  return diff
-  
-  #v = tf.matmul(tf.matmul(diff, inv_cov), tf.transpose(diff))
+  diff = observations - mean
+  v = tf.diag_part(tf.matmul(tf.matmul(diff, inv_cov), tf.transpose(diff)))
 
-  #return -c * (k * log2pi + log_det_cov + v)
+  return -c * (k * log2pi + log_det_cov + v)
 
 # observations - N x N Matrix observations to used in calculating probability
 # return - N unnormalized gaussian probabilities vector
 def unnormalized_gaussians(observations, mean, covariance):
-  obs = np.asmatrix(observations)
-  obs_len = obs.shape[1]
-  probabilities = []
-  
-  for i in range(obs_len):      
-    probabilities.append(np.exp(log_probability(observations[:, i], mean, covariance, 0.25)))
-  
-  probabilities = np.asarray(probabilities)
-  return probabilities
+  #covariance = tf.matmul(tf.transpose(covariance), covariance)
+  return tf.exp(log_probability(observations, mean, covariance, 0.25))
 
 def get_cosine(G, alphas):
-  return (1 - (alphas[0] ** 2) - (alphas[1] ** 2)) / (2 * alphas[0] * alphas[1] * np.sum(G[0] * G[1]))
+  return (1 - (alphas[0] ** 2) - (alphas[1] ** 2)) / (2 * alphas[0] * alphas[1] * tf.reduce_sum(G[0] * G[1]))
 
-def quantum_gmm(observations, alphas, means, covariances):
+def quantum_gmm(observations, alphas, means, covariances, num_components):
   G = []
-  for i in range(len(alphas)):
+  
+  for i in range(num_components):
     G.append(unnormalized_gaussians(observations, means[i], covariances[i]))
-  G = np.asarray(G)
+  G = tf.convert_to_tensor(G, dtype=tf.float32)
 
   prob_sum = 0
-  print(get_cosine(G, alphas))
-  for i in range(len(alphas)):
+  
+  for i in range(num_components):
     prob_sum += (alphas[i] ** 2) * (G[i] ** 2) + (alphas[0] * alphas[1] * get_cosine(G, alphas) * G[0] * G[1])
   
-  # Normalize the probability.
-  prob_sum /= np.sum(prob_sum)
-
   return prob_sum
 
-def constraint(observations, alphas, means, covariances):
+def constraint(observations, alphas, means, covariances, num_components):
   G = []
-  for i in range(len(alphas)):
+  for i in range(num_components):
     G.append(unnormalized_gaussians(observations, means[i], covariances[i]))
+  G = tf.convert_to_tensor(G, dtype=tf.float32)
 
-  return (alphas[0] ** 2) + (alphas[1] ** 2) + 2 * alphas[0] * alphas[1] * get_cosine(G, alphas) * np.sum(G[0] * G[1])
+  return (alphas[0] ** 2) + (alphas[1] ** 2) + 2 * alphas[0] * alphas[1] * get_cosine(G, alphas) * tf.reduce_sum(G[0] * G[1])
 
 # Calculate responsibilities.
-def get_Q(G, alphas):
-  o = (G[0] * G[1]) / np.sum(G[0] * G[1])
+def get_Q(G, alphas, num_components):
+  o = (G[0] * G[1]) / tf.reduce_sum(G[0] * G[1])
   alphao = (1 - (alphas[0] ** 2) - (alphas[1] ** 2)) * o
 
   Q = []
   common_divisor = (alphas[0] ** 2) * (G[0] ** 2) + (alphas[1] ** 2) * (G[1] ** 2) + alphao
-  for i in range(len(alphas)):
+  for i in range(num_components):
     Q.append((alphas[i] ** 2) * (G[i] ** 2) + 0.5 * alphao)
   Q /= common_divisor
+  Q = tf.convert_to_tensor(Q, dtype=tf.float32)
 
   return Q
+
+def assemble_covs(covs_lower, num_components):
+  covs = []
+  for i in range(num_components):
+    covs.append(tf.matmul(tf.transpose(covs_lower[i]), covs_lower[i]))
+  covs = tf.convert_to_tensor(covs, dtype=tf.float32)
+
+  return covs
